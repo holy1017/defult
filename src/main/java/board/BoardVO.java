88a630c53d3df02c9f02fdc2364cd078e7b172ef
holy <holy1017@naver.com>
2015-10-29 12:19:18 +0900

    VO 생성 밑 테이블 수정 아놔 수정 날자 빼먹었어

---
+++ src/main/java/board/BoardVO.java
@@ -0,0 +1,59 @@
+package board;
+
+public class BoardVO {
+
+	private int b_no;
+	private String b_title;
+	private String b_content;
+	private String b_indate;
+	private String b_eddate;
+	private int u_no;
+
+	public int getB_no() {
+		return b_no;
+	}
+
+	public void setB_no(int b_no) {
+		this.b_no = b_no;
+	}
+
+	public String getB_title() {
+		return b_title;
+	}
+
+	public void setB_title(String b_title) {
+		this.b_title = b_title;
+	}
+
+	public String getB_content() {
+		return b_content;
+	}
+
+	public void setB_content(String b_content) {
+		this.b_content = b_content;
+	}
+
+	public String getB_indate() {
+		return b_indate;
+	}
+
+	public void setB_indate(String b_indate) {
+		this.b_indate = b_indate;
+	}
+
+	public String getB_eddate() {
+		return b_eddate;
+	}
+
+	public void setB_eddate(String b_eddate) {
+		this.b_eddate = b_eddate;
+	}
+
+	public int getU_no() {
+		return u_no;
+	}
+
+	public void setU_no(int u_no) {
+		this.u_no = u_no;
+	}
+}
