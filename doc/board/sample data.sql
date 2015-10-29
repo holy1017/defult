c3892317e3a46b6756cea7bb8b4a8f94d130c404
holy <holy1017@naver.com>
2015-10-29 12:28:00 +0900

    샘플 데이터 생성 완료 vo 누락된거 완료

---
+++ doc/board/sample data.sql
@@ -0,0 +1,9 @@
+/* Formatted on 2015-10-29 오후 12:26:31 (QP5 v5.227.12220.39754) */
+INSERT INTO defult_user (u_id, u_pw, u_nick)
+     VALUES ('test', 'test', 'test');
+
+INSERT INTO defult_board (b_title, b_content, u_no)
+     VALUES ('test', 'test', 1);
+
+INSERT INTO defult_reply (r_content, b_no, u_no)
+     VALUES ('test', 1, 1);
--- src/main/java/board/ReplyVO.java
+++ src/main/java/board/ReplyVO.java
@@ -2,4 +2,59 @@ package board;
 
 public class ReplyVO {
 
+	private int r_no;
+	private String r_content;
+	private int b_no;
+	private int u_no;
+	private String r_indate;
+	private String r_eddate;
+
+	public int getR_no() {
+		return r_no;
+	}
+
+	public void setR_no(int r_no) {
+		this.r_no = r_no;
+	}
+
+	public String getR_content() {
+		return r_content;
+	}
+
+	public void setR_content(String r_content) {
+		this.r_content = r_content;
+	}
+
+	public int getB_no() {
+		return b_no;
+	}
+
+	public void setB_no(int b_no) {
+		this.b_no = b_no;
+	}
+
+	public int getU_no() {
+		return u_no;
+	}
+
+	public void setU_no(int u_no) {
+		this.u_no = u_no;
+	}
+
+	public String getR_indate() {
+		return r_indate;
+	}
+
+	public void setR_indate(String r_indate) {
+		this.r_indate = r_indate;
+	}
+
+	public String getR_eddate() {
+		return r_eddate;
+	}
+
+	public void setR_eddate(String r_eddate) {
+		this.r_eddate = r_eddate;
+	}
+
 }
